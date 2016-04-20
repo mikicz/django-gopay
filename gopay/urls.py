630cfbadf12ad382ba8ca0cc3051cc1a7a3cfa98
Mikuláš Poul <mikulaspoul@gmail.com>
2016-04-20 14:52:47 +0200

    Update urls.py
    Django 1.6 compabilitiy

--- gopay/urls.py
+++ gopay/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('gopay.views',
     url(r'^success$', 'gopay_success', name='gopay_success'),
